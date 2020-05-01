00a77e2cea2ece6c0205f5c8c2b8cf3caf1a6ece
Taesun Lee <superbsun@gmail.com>
2020-05-01 22:02:09 +0900

    add client side cache for like button

--- src/dist/apollo.dev.js
+++ src/dist/apollo.dev.js
@@ -10,7 +10,27 @@ var _apolloBoost = _interopRequireDefault(require("apollo-boost"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var client = new _apolloBoost["default"]({
-  uri: "http://localhost:4000/"
+  uri: "http://localhost:4000/",
+  resolvers: {
+    Movie: {
+      isLiked: function isLiked() {
+        return false;
+      }
+    },
+    Mutation: {
+      toggleLikeMovie: function toggleLikeMovie(_, _ref, _ref2) {
+        var id = _ref.id,
+            isLiked = _ref.isLiked;
+        var cache = _ref2.cache;
+        cache.writeData({
+          id: "Movie:".concat(id),
+          data: {
+            isLiked: !isLiked
+          }
+        });
+      }
+    }
+  }
 });
 var _default = client;
 exports["default"] = _default;
